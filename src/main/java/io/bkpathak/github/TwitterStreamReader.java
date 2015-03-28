e5bfcfc5ee240598c728adc534327cb457abcd9d
bkpathak <bkpathak@mtu.edu>
2015-03-27 17:18:23 -0700

    twitter stream producer

--- src/main/java/io/bkpathak/github/TwitterStreamReader.java
+++ src/main/java/io/bkpathak/github/TwitterStreamReader.java
@@ -1,7 +1,77 @@
 package io.bkpathak.github;
 
+import com.twitter.hbc.ClientBuilder;
+import com.twitter.hbc.core.Constants;
+import com.twitter.hbc.core.endpoint.StatusesSampleEndpoint;
+import com.twitter.hbc.core.processor.StringDelimitedProcessor;
+import com.twitter.hbc.httpclient.BasicClient;
+import com.twitter.hbc.httpclient.auth.Authentication;
+import com.twitter.hbc.httpclient.auth.OAuth1;
+import kafka.javaapi.producer.Producer;
+import kafka.message.Message;
+import kafka.producer.KeyedMessage;
+import kafka.producer.ProducerConfig;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.util.Properties;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by bijay on 3/27/15.
  */
 public class TwitterStreamReader {
+  private final static String topic = "realtimetweets";
+
+  public static void run(String consumerKey, String consumerSecret, String token, String secret) throws InterruptedException, UnsupportedEncodingException {
+
+    Properties properties = new Properties();
+    properties.put("metadata.broker.list", "localhost:9092");
+    properties.put("serializer.class", "kafka.serializer.StringEncoder");
+    Producer<String, Message> producer;
+
+    // Create an appropriately sized blocking queue
+    BlockingQueue<String> queue = new LinkedBlockingQueue<String>(10000);
+
+    // Define our endpoint: By default, delimited=length is set (we need this for our processor)
+    // and stall warnings are on.
+    StatusesSampleEndpoint endpoint = new StatusesSampleEndpoint();
+    endpoint.stallWarnings(false);
+
+    Authentication auth = new OAuth1(consumerKey, consumerSecret, token, secret);
+
+    // Create a new BasicClient. By default gzip is enabled.
+    BasicClient client = new ClientBuilder()
+            .name("sampleExampleClient")
+            .hosts(Constants.STREAM_HOST)
+            .endpoint(endpoint)
+            .authentication(auth)
+            .processor(new StringDelimitedProcessor(queue))
+            .build();
+    // Establish a connection
+    client.connect();
+
+    //initialize producer
+    ProducerConfig config = new ProducerConfig(properties);
+    producer = new Producer<String, Message>(config);
+
+    // Do whatever needs to be done with messages
+    for (int msgRead = 0; msgRead < 1000; msgRead++) {
+      if (client.isDone()) {
+        System.out.println("Client connection closed unexpectedly: " + client.getExitEvent().getMessage());
+        break;
+      }
+      Message message = new Message(queue.take().getBytes("UTF-8"));
+      if (message == null) {
+        System.out.println("No messages!!!");
+      } else {
+        producer.send(new KeyedMessage<String, Message>(topic, message));
+      }
+    }
+    client.stop();
+    // Print some stats
+    System.out.printf("The client read %d messages!\n", client.getStatsTracker().getNumMessages());
+  }
 }
